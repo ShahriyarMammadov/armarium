9e3f726a76224fe8f62a7b5cfa9bd153db6d0fe2
Şəhriyar Məmmədov <76937208+Sehriyar4473@users.noreply.github.com>
2023-08-27 23:45:01 +0400

    responsive fixed +

--- client/src/pages/modelsDetailPage/index.jsx
+++ client/src/pages/modelsDetailPage/index.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import Zoom from "react-medium-image-zoom";
-import "react-medium-image-zoom/dist/styles.css";
 import { Image } from "antd";
 import "./index.scss";
 import coverImage from "../../assets/images/101.jpg";
