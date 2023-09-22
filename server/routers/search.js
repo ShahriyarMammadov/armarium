acc84b6a1425c55d339e6bd4f67856c7572be34e
Şəhriyar Məmmədov <76937208+Sehriyar4473@users.noreply.github.com>
2023-09-22 14:31:37 +0400

    change

--- server/routers/search.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { searchByName } from "../controllers/search.js";
-
-const searchRouter = Router();
-
-// SEARCH
-searchRouter.post("/searchByName/:value", searchByName);
-
-export default searchRouter;
