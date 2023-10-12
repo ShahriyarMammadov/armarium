b9e72aa2e8f1f8f15d71bfdc997db32a7554249b
Şəhriyar Məmmədov <76937208+Sehriyar4473@users.noreply.github.com>
2023-10-12 10:36:08 +0400

    colorchange

--- src/components/models/index.jsx
+++ src/components/models/index.jsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./index.scss";
-import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import SkeletonComponent from "../skeleton";
 import { useTranslation } from "react-i18next";
